755a61d03f527c2db82b78abb207206ae923443a
Amonwan <156374482+Amonwn@users.noreply.github.com>
2024-11-03 20:54:25 -0500

    added langchain codes

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,7 @@ os.environ["OPENAI_API_KEY"] = my_secret_key
 
 prompt = st.text_input("We want to hear from you!", " ")
 
+"""
 ### OpenAI model
 client = OpenAI()
 response = client.chat.completions.create(
@@ -24,3 +25,14 @@ response = client.chat.completions.create(
 st.write(
     response.choices[0].message.content
 )
+"""
+
+from langchain.llms import OpenAI
+from langchain_core.output_parsers import StrOutputParser
+
+
+### Create the LLM API object
+llm = OpenAI(openai_api_key=openai_api_key)
+# llm = ChatOpenAI(openai_api_key=openai_api_key, model="gpt-4")
+
+
