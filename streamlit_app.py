fdf657cfad16aa599577dfb0b0711cbd1e7dfd9c
Amonwan <156374482+Amonwn@users.noreply.github.com>
2024-11-04 10:32:00 -0500

    text

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ llm = OpenAI(openai_api_key=my_secret_key)
 # llm = ChatOpenAI(openai_api_key=openai_api_key, model="gpt-4")
 
 ### Create a template to handle the case
-airline_template = """You are an expert at airline's customer services.
+airline_template = """You are a professional customer service team at airline's customer services.
 From the following text, determine whether the user's experiences is positive or negative.
 If negative, determine whether the negative experiences caused by the airline(e.g., lost luggage) 
 or beyond the airline's control(e.g., a weather-related delay).
@@ -41,7 +41,7 @@ experience_type_chain = (
 
 #2nd chain
 negative_airline_fault_chain = PromptTemplate.from_template(
-    """You are an expert at airline's customer services. \
+    """You are professional customer service team at airline's customer services. \
 If their dissatisfaction is the airline's fault from the following text,respond with showing sympathies and 
 inform the user that customer service team will contact them for further information.
 Respond professionally as an airline's customer services. Respond in first-person mode but exclude your name.
@@ -60,7 +60,7 @@ Text:
 
 #3rd chain
 negative_not_airline_fault_chain = PromptTemplate.from_template(
-    """You are an expert at airline's customer services. \
+    """You are professional customer service team at airline's customer services. \
 If their dissatisfaction is beyond the airline's control from the following text, respond with reasoning.
 Respond professionally as an airline's customer services. Respond in first-person mode but exclude your name.
 
