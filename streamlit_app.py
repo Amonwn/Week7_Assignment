822595e2b0bebfc84100eb3c6d12919fe6c2564f
Amonwan <156374482+Amonwn@users.noreply.github.com>
2024-11-04 09:34:35 -0500

    added template

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,24 @@ my_secret_key = st.secrets['MyOpenAIKey']
 os.environ["OPENAI_API_KEY"] = my_secret_key
 
 prompt = st.text_input("Your experiences are valuable to us", " ")
+
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain_core.output_parsers import StrOutputParser
+
+### Create the LLM API object
+llm = OpenAI(openai_api_key=my_secret_key, model="gpt-4")
+# llm = ChatOpenAI(openai_api_key=openai_api_key, model="gpt-4")
+
+### Create a template to handle the case
+airline_template = """You are an expert at airline's customer services.
+From the following text, determine whether the user's experiences is positive or negative.
+If negative, determine whether the negative experiences caused by the airline(e.g., lost luggage) 
+or beyond the airline's control(e.g., a weather-related delay).
+
+Text:
+{request}
+
+"""
