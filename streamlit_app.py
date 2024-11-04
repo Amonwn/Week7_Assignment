a848e1b23f5fd804da7cbbc2b6ca3efdbdff2115
Amonwan <156374482+Amonwn@users.noreply.github.com>
2024-11-03 20:39:13 -0500

    added Openai secret key and fundamental codes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,23 @@
 import streamlit as st
+from openai import OpenAI
+import os
 
-st.title("🎈 My new app")
+st.title("Share with us your experience of the latest trip")
+
+### Load your API Key
+os.environ["OPENAI_API_KEY"] = st.secrets["OpenAIkey"]
+
+### OpenAI stuff
+client = OpenAI()
+response = client.chat.completions.create(
+  model="gpt-4o-mini",
+  messages=[
+    {"role": "system", "content": "Complete the following prefix"},
+    {"role": "user", "content": prompt}
+  ],
+)
+
+### Display
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    response.choices[0].message.content
 )
